843dd43446310a3f65272f13ecc4e7f6deb28871
ethan martin <ethan@ethans-mbp.home>
2024-08-17 21:29:17 +0930

    added skill entity and fixed visibility levels of classes

--- src/IronLeg.Domain/Entities/SkillEntity.cs
+++ src/IronLeg.Domain/Entities/SkillEntity.cs
@@ -1,11 +1,9 @@
 namespace IronLeg.Domain.Entities
 {
-  public sealed class TaskEntity : SoftDeletableEntity
+  public sealed class SkillEntity : SoftDeletableEntity
   {
     public required string Title { get; set; }
     public required string Colour { get; set; }
     public Guid? ParentId { get; set; }
-    public Guid? SkillId { get; set; }
-    public Guid? ClientId { get; set; }
   }
 }
