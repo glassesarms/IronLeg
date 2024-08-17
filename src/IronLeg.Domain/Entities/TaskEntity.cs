2b1c830ffbae7bcde56eb4fc9372811c25740a3a
ethan martin <ethan@ethans-mbp.home>
2024-08-17 22:00:59 +0930

    change name of client to subject, add status

--- src/IronLeg.Domain/Entities/TaskEntity.cs
+++ src/IronLeg.Domain/Entities/TaskEntity.cs
@@ -3,9 +3,11 @@ namespace IronLeg.Domain.Entities
   public sealed class TaskEntity : SoftDeletableEntity
   {
     public required string Title { get; set; }
+    // Todo: create task status enum
+    public int Status { get; set; }
     public required string Colour { get; set; }
     public Guid? ParentId { get; set; }
     public Guid? SkillId { get; set; }
-    public Guid? ClientId { get; set; }
+    public Guid? SubjectId { get; set; }
   }
 }
