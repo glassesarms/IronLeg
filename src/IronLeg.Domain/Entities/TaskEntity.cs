761cb321abf4af377c0ca3caf9bc7e0baec76c93
ethan martin <ethan@ethans-MacBook-Pro.local>
2024-08-17 21:07:14 +0930

    added task entity

--- src/IronLeg.Domain/Entities/TaskEntity.cs
+++ src/IronLeg.Domain/Entities/TaskEntity.cs
@@ -3,5 +3,9 @@ namespace IronLeg.Domain.Entities
   public sealed class TaskEntity
   {
     public required string Title { get; set; }
+    public required string Colour { get; set; }
+    public Guid? ParentId { get; set; }
+    public Guid? SkillId { get; set; }
+    public Guid? ClientId { get; set; }
   }
 }
